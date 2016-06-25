507d26ba87bb53d709b7bf8a0a2f951e21c509f0
Jonathan McCaffrey <jon.mcc_social@live.com>
2016-06-25 13:58:20 -0400

    Committing Floor

---
+++ StayAwhile/Assets/Scripts/Systems/Modules/ModuleSystem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ModuleSystem : MonoBehaviour {
+
+	private string filePath = "Assets/Resources/Prefabs/Modules";
+
+	private static ModuleSystem instance = null;
+	private static ModuleSystem get() {
+		if (instance == null) {
+
+			GameObject found = GameObject.Find ("ModuleSystem");
+
+			if (found) {
+				instance = found.GetComponent<ModuleSystem> ();
+			}
+
+			if (instance == null) {
+				GameObject created = Resources.Load<GameObject> ("Prefabs/Modules/ModuleSystem");
+				instance = created.GetComponent<ModuleSystem> ();
+			}
+		}
+
+		return instance;
+	}
+
+	public void Start() {
+		generateNext (-1);
+	}
+
+
+	List<TileRoot> roots = new List<TileRoot>();
+
+	int depth = -1;
+
+	public static void generateNext(int depth) {
+		while (get().depth - 4 <= depth) {
+
+			get ().depth++;
+
+			GameObject area = Instantiate(Resources.Load<GameObject> ("Prefabs/Modules/1"));
+			if (area) {
+			//	area.transform.parent = get ().gameObject.transform;
+				area.transform.parent = get().gameObject.transform;
+
+				area.transform.position = new Vector3 (0, 0, get ().depth * 70);
+
+				Debug.Log ("aasdasd");
+
+				area.GetComponent<TileRoot> ().depth = get ().depth;
+
+				get ().roots.Add (area.GetComponent<TileRoot> ());
+			}
+
+		}
+
+	}
+}
---
+++ StayAwhile/Assets/Scripts/Systems/Tiles/TileRoot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+public class TileRoot : MonoBehaviour {
+
+	public int depth = 0;
+
+	void OnTriggerEntered(Collider col) {
+		ModuleSystem.generateNext (depth );
+	}
+
+}
