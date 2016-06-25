11669fa66a9dd8285133efa6876f9e83dd889f8b
Patrick Boyer <boyer.patrick@gmail.com>
2016-06-24 20:38:08 -0400

    basic "input on beat" detection

---
+++ StayAwhile/Assets/Scripts/DebugBeatDetect.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class DebugBeatDetect : MonoBehaviour, AudioProcessor.AudioCallbacks
+{
+    private int beatCounter = 0;
+    private float beatTimer;
+    private float beatTolerance = 0.3f;
+    public void onOnbeatDetected()
+    {
+        beatCounter++;
+        beatTimer = 0f;
+        Debug.Log("Beat " + beatCounter);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fired on beat");
+        }
+
+
+    }
+
+    public void onSpectrum(float[] spectrum)
+    {
+
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+        processor.addAudioCallback(this);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        beatTimer += Time.deltaTime;
+	    if (Input.GetButtonDown("Fire1") && beatTimer <= beatTolerance)
+        {
+            Debug.Log("Fired on beat!");
+        }
+        if (Input.GetButtonDown("Fire1") && beatTimer > beatTolerance)
+        {
+            Debug.Log("Fired too late");
+        }
+    }
+}
