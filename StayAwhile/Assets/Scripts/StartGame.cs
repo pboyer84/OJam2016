40a715453122813b9b21f601737e086dfe2cd02d
Patrick Boyer <boyer.patrick@gmail.com>
2016-06-26 14:25:01 -0400

    a start screen

---
+++ StayAwhile/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("audiotest");
+        }
+	}
+}
