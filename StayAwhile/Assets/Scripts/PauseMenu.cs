bff4e159ceaef4eda0deb81a9a3ee001171a77b8
Patrick Boyer <boyer.patrick@gmail.com>
2016-06-26 10:31:58 -0400

    rudimentary escape menu

---
+++ StayAwhile/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject PausePanel;
+
+    void Awake()
+    {
+        PausePanel = GameObject.Find("PausePanel");
+        PausePanel.SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetButtonDown("Cancel"))
+        {
+            PausePanel.SetActive(!PausePanel.activeSelf);
+        }
+	}
+
+    public void OnRestartButtonClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
